78354cbaa9b0ce4c2b0cd3a504e923242691225a
batuhan yıldırım <yildirimbatu52@gmail.com>
2024-07-18 00:44:15 +0300

    clean code

--- src/List.js
+++ src/List.js
@@ -5,9 +5,7 @@ const Done = (props) => {
   const {data} = props
 
   return (
-    <TableRow
-      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-    >
+    <TableRow>
       <TableCell component="th" scope="row">
         {data.status === 'Done' &&
           <>
